d90f6e1830e88a0e0994997e4f601e9e6036bea5
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-07 06:37:56 +0800

    this is demo of push  sample first and second scripts

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,6 +1,11 @@
-date1 = $[date+%F]
+a=20
+the value of a = $a
+
+date2 = $[date+%F]
+today's date = $date2
 
 
-the value for =$[2+3-4+8*89]
+the value  =$[2+3-4+8*89]
+the data = $the value
 
 echo -e "hello\npeople"
