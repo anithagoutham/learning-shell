ffcc6a40996edf8b9e658966e15f375c1d51c7cf
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-09 11:10:08 +0800

    this is demo of push  sample first and second scripts

---
+++ sample.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+## Declare a function
+sample() {
+  echo sample function
+  echo a from main program = $a
+  b=20
+}
+
+## Access a function, simply function name
+a=10
+sample
+echo b from function = $b
