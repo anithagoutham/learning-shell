03f93855ebb7e260e5ce5018fdf09561a63c78c3
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-12 15:29:12 +0800

    this is demo of push  sample first and second scripts

--- variable.sh
+++ variable.sh
@@ -5,3 +5,10 @@ echo first argument = $1
 echo second argument =$2
 echo all arguments = $@
 echo number of arguments = $#
+
+sample(){
+  echo first argument in function =$1
+  echo second argument in function = $2
+
+}
+sample 10 22 30 44
